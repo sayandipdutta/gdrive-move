15512450413550180a02985cfbaa3e881f820df5
sayandip199309@gmail.com <sayandip199309@gmail.com>
2022-08-05 02:31:13 +0530

    refactor: Load sourse, destination from config

--- internal/main.py
+++ internal/main.py
@@ -1,13 +1,14 @@
+from . import config
 from .datatypes import Unit
 from .service import DriveService
 
-SOURCE = "1XvhVCE1s1uRZgx3fFTnKITPTXszVZ1eC"
-DESTINATION = "17V7dcxuSPiMWqOYhDKc-_PBi-Hhq-Ogd"
+SOURCE = config['DEFAULT']['source']
+DESTINATION = config['DEFAULT']['destination']
 
 with DriveService() as gdrive:
     items = gdrive.list_dir(SOURCE, log=True)
-    clusters = gdrive.make_cluster(items, upper_limit=3 * Unit.TB, exclude={'Move_Series_1'}, max_clusters=1)
-    new_folder = gdrive.create_folder("Move_Series_1", destination=SOURCE)
+    clusters = gdrive.make_cluster(items, upper_limit=3 * Unit.TB, exclude={'Temporary'}, max_clusters=1)
+    new_folder = gdrive.create_folder("Temporary", destination=SOURCE)
     fid, name = new_folder.id, new_folder.name
     for cluster in clusters:
         cluster = next(clusters)
